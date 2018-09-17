0b3d17f50b396a4f3345fb9f95c703cf78af66eb
ke791070 <ke791070@C39386.ens.uqam.ca>
2018-09-17 11:24:30 -0400

    salutation a prisca

--- NetBeansProjects/JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ NetBeansProjects/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -9,7 +9,7 @@ public class JavaApplication3 {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello capris");
+        System.out.println("Bonjour prisca petelo");
         
        
        Person petelo=new Person("prisca","petelo",22); 
