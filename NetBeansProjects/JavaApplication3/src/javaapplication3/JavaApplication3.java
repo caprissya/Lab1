084c759dd512b2c19f31901bfd00d5bd621203a2
ke791070 <ke791070@C39386.ens.uqam.ca>
2018-09-17 11:21:25 -0400

    depot initial

---
+++ NetBeansProjects/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,22 @@
+    
+package javaapplication3;
+
+import aleatoire.Person;
+
+public class JavaApplication3 {
+
+ 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello capris");
+        
+       
+       Person petelo=new Person("prisca","petelo",22); 
+        
+        
+        
+        
+    }
+    
+}
